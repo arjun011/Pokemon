2ede025f419ad4194c453843f86f907fb523497c
Arjun Patel <arjun.patel@nascentinfo.com>
2022-07-18 14:14:45 +0100

    Initial PokemonListView

---
+++ Pokemon/Home/PokemonListView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonListView.swift
+//  Pokemon
+//
+//  Created by Arjun on 18/07/22.
+//
+
+import SwiftUI
+
+struct PokemonListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PokemonListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonListView()
+    }
+}
---
+++ Pokemon/Home/PokemonListViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  PokemonListViewModel.swift
+//  Pokemon
+//
+//  Created by Arjun on 18/07/22.
+//
+
+import Foundation
+class PokemonListViewModel: ObservableObject {
+    
+}
