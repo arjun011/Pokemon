1a8445201ea82b036d63dd93003ae736ab465d5d
Arjun Patel <arjun.patel@nascentinfo.com>
2022-07-18 18:04:58 +0100

    Create PokemonListClient

---
+++ Pokemon/Home/PokemonListClient.swift
@@ -0,0 +1,32 @@
+//
+//  PokemonListClient.swift
+//  Pokemon
+//
+//  Created by Arjun on 18/07/22.
+//
+
+import Foundation
+
+class PokemonListClient {
+    
+    /// Retrive pokemonList
+    /// - Returns: PokemonListModel
+    func retrivePokemonList() async throws -> ResponseManager<PokemonListModel> {
+        
+        let getPokemonList = APIConstant.getPokemonList
+        debugPrint(getPokemonList)
+        
+        do {
+            let request = try ClientManager.GET(getPokemonList)
+            let (data,response) = try await URLSession.shared.data(for: request)
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw ValidationError.invalidServerResponse }
+            
+            let session = try JSONDecoder().decode(PokemonListModel.self, from: data)
+            
+            return ResponseManager.success(session)
+        }catch {
+            throw ValidationError.invalidServerResponse
+        }
+    }
+}
