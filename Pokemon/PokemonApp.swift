fd304ee772ef7633acf0c0a5ce5b75d8c4fcbe12
Arjun Patel <arjun.patel@nascentinfo.com>
2022-07-18 11:59:34 +0100

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Arjun on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
